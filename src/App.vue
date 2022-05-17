c7b880b43a4401cbf03c84ac4b23e7922a1ad920
Gabriel Forti Zonaro <zonaro@outlook.com>
2022-05-17 00:36:04 -0300

    -

--- src/App.vue
+++ src/App.vue
@@ -52,11 +52,7 @@ export default {
 
     generateTemplate() {
       this.calendarioTemplate += 1;
-      html2canvas(document.querySelector("#maincontent")).then(canvas => {
-        let img = canvas.toDataURL("image/png");
-        console.log(img)
-        document.getElementById("link_download").setAttribute("href", img);
-      });
+ 
     },
   },
 
